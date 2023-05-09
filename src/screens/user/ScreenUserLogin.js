a8a1cf09d1253c5c2ff1398f608e0950075ab536
Yanzen Project <yanwarsolah@gmail.com>
2023-05-09 21:03:10 +0700

    step 01

---
+++ src/screens/user/ScreenUserLogin.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const ScreenUserLogin = ({ navigation }) => {
+  // TODO: tambahkan context
+  // const [, setIsAuthenticated] = useContext(ContextUserAuthentication)
+  const [user, setUser] = useState({}); // TODO: add schema user
+  const [complete, setComplete] = useState(false);
+};
+
+export default ScreenUserLogin;
