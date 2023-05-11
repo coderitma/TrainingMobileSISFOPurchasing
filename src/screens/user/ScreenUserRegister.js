2a58e6f29883c1b07da84b1c57f3f5515f7a5b0c
Yanzen Project <yanwarsolah@gmail.com>
2023-05-11 19:12:01 +0700

    step 01/37

--- src/screens/user/ScreenUserRegister.js
+++ src/screens/user/ScreenUserRegister.js
@@ -1,12 +1,34 @@
 import _ from "lodash";
 import { useEffect, useState } from "react";
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import { Alert, SafeAreaView, ScrollView, Text, View } from "react-native";
 import WidgetBaseLogo from "../../widgets/base/WidgetBaseLogo";
 import { Button, TextInput } from "react-native-paper";
+import { ServiceUserRegister } from "../../services/ServiceUser";
 
 const ScreenUserRegister = ({ navigation }) => {
-  const [user, setUser] = useState({}); // TODO: add schema user
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  }); // TODO: add schema user
   const [complete, setComplete] = useState(false);
+
+  const handleChange = (name, value) => {
+    setUser((values) => ({ ...values, [name]: value }));
+  };
+
+  const userRegister = () => {
+    ServiceUserRegister(user)
+      .then((data) => {
+        console.log(data);
+        Alert.alert("Berhasil", "Anda berhasil register, silahkan login.");
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     setComplete(false);
@@ -37,16 +59,32 @@ const ScreenUserRegister = ({ navigation }) => {
               style={{ flex: 1 }}
               label="First Name"
               placeholder="Masukan nama depan"
+              value={user.firstName}
+              onChangeText={(text) => handleChange("firstName", text)}
             />
             <TextInput
               style={{ flex: 1 }}
               label="Last Name"
               placeholder="Masukan nama belakang"
+              value={user.lastName}
+              onChangeText={(text) => handleChange("lastName", text)}
             />
           </View>
-          <TextInput label="Email" placeholder="Masukan email" />
-          <TextInput label="Password" />
-          <Button mode="contained">Register</Button>
+          <TextInput
+            label="Email"
+            placeholder="Masukan email"
+            value={user.email}
+            onChangeText={(text) => handleChange("email", text)}
+          />
+          <TextInput
+            label="Password"
+            value={user.password}
+            onChangeText={(text) => handleChange("password", text)}
+            secureTextEntry={true}
+          />
+          <Button onPress={userRegister} mode="contained">
+            Register
+          </Button>
           <Button
             onPress={() => {
               navigation.goBack();
